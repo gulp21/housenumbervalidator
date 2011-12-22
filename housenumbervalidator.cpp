9d701d27a5eaed832c5eedadde60d4c2e1aef159
gulp21 <support.gulp21@googlemail.com>
2011-12-22 14:48:01 +0100

    working code, unfortunately not working with large files

--- housenumbervalidator.cpp
+++ housenumbervalidator.cpp
@@ -23,34 +23,74 @@
 #include <QFile>
 #include <QList>
 #include <QString>
+#include <QStringList>
 #include <QTime>
 
 using namespace std;
 
+struct housenumber;
+
 struct housenumber {
 	double lat, lon;
-	int id, dupeId;
-	QString name, shop, housenumber, street, postcode, city, country;
+	int id;
+	bool fixme, isWay, dupeIsWay;
+	QString name, shop, number, street, postcode, city, country;
+	housenumber* dupe;
 };
 
-QString filename="input.osm";
+QString qsAssumeCountry="", qsAssumeCity="", qsAssumePostcode="", filename="input.hnr.osm";
+bool bIgnoreFixme=true;
 
-QList<housenumber> qlHousenumbers, qlDupes;
+QList<housenumber> qlHousenumbers, qlDupes, qlNodes;
 
 bool bFindDupe(housenumber &hnr);
+bool isComplete(housenumber hnr);
+QString qsGenerateOutput(housenumber hnr);
+void vGetLatLonForWay(housenumber &hnr, int ref);
 
 int main(int argc, const char* argv[]){ 
 	QTime now;
 	now.start();
-	// function compare(a, b){
-	// 	a=explode(" ", a);
-	// 	b=explode(" ", b);
-	// 	//compare postcodes
-	// 	if(a[5] == b[5]) return 0;
-	// 	return (a[5] < b[5]) ? -1 : 1;
-	// }
+        
 	if(argc>1) {
+		if(QString(argv[1])=="--help" || QString(argv[1])=="-h") {
+			qDebug() << "Usage: ./housenumbervalidator [FILENAME.hnr.osm [OPTIONS]]\n";
+			qDebug() << "Options:";
+			qDebug() << "  -ac=XX  --assume-country=XX   When a housenumber does not have a addr:country value, it is set to XX";
+			qDebug() << "  -aci=XX --assume-city=XX";
+			qDebug() << "  -ap=XX  --assume-postcode=XX";
+			qDebug() << "  -nif,   --not-ignore-fixme    do output ways/nodes which have a fixme tag";
+			qDebug() << "  -h      --help                Print this help";
+			
+			return 0;
+		}
+		
 		filename=QString(argv[1]);
+		
+		for(int i=2; i<argc; i++){
+			if(QString(argv[i])=="-nif" || QString(argv[i])=="--not-ignore-fixme") bIgnoreFixme=false;
+			/*else if(QString(argv[i])=="-in" or QString(argv[i])=="--ignore-note") ignorenote=TRUE;
+			else if(QString(argv[i])=="-npc" or QString(argv[i])=="--no-postcode-count") nopostcodecount=TRUE;
+			else if(QString(argv[i])=="-ich" or QString(argv[i])=="--ignore-country-hint") ignorecountryhint=TRUE;
+			else if(QString(argv[i])=="-iih" or QString(argv[i])=="--ignore-city-hint") ignorecityhint=TRUE;
+			else if(QString(argv[i])=="-iph" or QString(argv[i])=="--ignore-postcode-hint") ignorepostcodehint=TRUE;
+			else if(QString(argv[i])=="-ish" or QString(argv[i])=="--ignore-street-hint") ignorestreethint=TRUE;
+			else if(QString(argv[i])=="-inh" or QString(argv[i])=="--ignore-number-hint") ignorenumberhint=TRUE;*/
+			else if(QString(argv[i]).contains("-ac=") || QString(argv[i]).contains("--assume-country=")) qsAssumeCountry=QString(argv[i]).split("=")[1];
+			else if(QString(argv[i]).contains("-aci=") || QString(argv[i]).contains("--assume-city=")) qsAssumeCountry=QString(argv[i]).split("=")[1];
+			else if(QString(argv[i]).contains("-ap=") || QString(argv[i]).contains("--assume-postcode=")) qsAssumeCountry=QString(argv[i]).split("=")[1];
+// 			else if(QString(argv[i]).contains("-wo=")) dupesperosm=QString(argv[i]).mid(4).toInt();
+// 			else if(QString(argv[i]).contains("--write-osm=")) dupesperosm=QString(argv[i]).mid(12).toInt();
+			else {
+				qDebug() <<  "unknown option " << argv[i];
+				return(1);
+			}
+		}
+	}
+	
+	if(!filename.endsWith(".hnr.osm")) {
+		qDebug("The filename should end with .hnr.osm\nPlease do NOT try to load LARGE files!\nUse ./filer to create a .hnr.osm file which contains only housenumbers.");
+		return -9;
 	}
 	
 	//open input file
@@ -59,7 +99,6 @@ int main(int argc, const char* argv[]){
 		qDebug() << "couldn't open" << filename;
 		return(2);
 	}
-	QTextStream in(&file);
 	
 	QFile duplicatesFile("dupes.txt");
 	duplicatesFile.remove();
@@ -68,8 +107,9 @@ int main(int argc, const char* argv[]){
 		file.close();
 		return(3);
 	}
+	QTextStream duplicatesStream(&duplicatesFile);
 	
-	QDomDocument doc("qcfx");
+	QDomDocument doc("osm");
 	
 	bool namespaceProcessing=false; QString errorMsg; int errorLine, errorColumn;
 	if(!doc.setContent(&file, namespaceProcessing, &errorMsg, &errorLine, &errorColumn )){
@@ -87,71 +127,136 @@ int main(int argc, const char* argv[]){
 		return -3;
 	}
 	
-	QDomNodeList dnlNodes=docElem.elementsByTagName("node");
-	QDomNodeList dnlWays=docElem.elementsByTagName("way");
-	
-	for(unsigned int i=0; i<dnlNodes.length(); i++) {
+	for(int l=0; l<2; l++) {
 		
-		QDomNode n=dnlNodes.at(i).firstChild();
+		QDomNodeList dnlNodes=docElem.elementsByTagName(l==0 ? "node" : "way");
 		
-		housenumber hnr;
-		hnr.id=dnlNodes.at(i).toElement().attribute("id","0").toInt();
-		hnr.lat=dnlNodes.at(i).toElement().attribute("lat","0").toInt();
-		hnr.lon=dnlNodes.at(i).toElement().attribute("lon","0").toInt();
-		
-		while(!n.isNull()) {
-			
-			QDomElement e=n.toElement(); // try to convert the node to an element
-			
-			if(!e.isNull()) {
-				
-				if(e.tagName()=="tag") {
-					if(e.attribute("k","")=="addr:country") {
-						hnr.country=e.attribute("v","");
-					} else if(e.attribute("k","")=="addr:city") {
-						hnr.city=e.attribute("v","");
-					} else if(e.attribute("k","")=="addr:postcode") {
-						hnr.postcode=e.attribute("v","");
-					} else if(e.attribute("k","")=="addr:street") {
-						hnr.street=e.attribute("v","");
-					} else if(e.attribute("k","")=="addr:housenumber") {
-						hnr.housenumber=e.attribute("v","");
-					}
-				}
-				
-			} // if(!e.isNull())
-			
-			n = n.nextSibling();
-			
-		} // while(!n.isNull())
-		
-		if(hnr.id!=0 && hnr.housenumber!="") {
-			if(bFindDupe(hnr)) {
-				qDebug() << "Found dupe!";
-				qlDupes.append(hnr);
-			} else {
-				qlHousenumbers.append(hnr);
-			}
-		} else {
-			//qDebug() << "There is some thing wrong with this element" << hnr.id << hnr.housenumber;
+		if(dnlNodes.length()==0) {
+			qDebug() << "No nodes?";
 		}
 		
-	} // for(dnlNodes)
+		for(unsigned int i=0; i<dnlNodes.length(); i++) {
+			
+			QDomNode n=dnlNodes.at(i).firstChild();
+			
+			housenumber hnr;
+			hnr.id=dnlNodes.at(i).toElement().attribute("id","0").toInt();
+			hnr.fixme=false;
+			if(l==0) {
+				hnr.lat=dnlNodes.at(i).toElement().attribute("lat","0").toDouble();
+				hnr.lon=dnlNodes.at(i).toElement().attribute("lon","0").toDouble();
+				if(n.isNull()) qlNodes.append(hnr);
+				hnr.isWay=false;
+			} else {
+				hnr.lat=-1;
+				hnr.isWay=true;
+			}
+			
+			while(!n.isNull()) {
+				
+				QDomElement e=n.toElement(); // try to convert the node to an element
+				
+				if(!e.isNull()) {
+					
+					if(e.tagName()=="tag") {
+						if(e.attribute("k",qsAssumeCountry)=="addr:country") {
+							hnr.country=e.attribute("v","");
+						} else if(e.attribute("k",qsAssumeCity)=="addr:city") {
+							hnr.city=e.attribute("v","");
+						} else if(e.attribute("k",qsAssumePostcode)=="addr:postcode") {
+							hnr.postcode=e.attribute("v","");
+						} else if(e.attribute("k","")=="addr:street") {
+							hnr.street=e.attribute("v","");
+						} else if(e.attribute("k","")=="addr:housenumber") {
+							hnr.number=e.attribute("v","");
+						} else if(e.attribute("k","")=="shop"
+						          || e.attribute("k","")=="amenity") {
+							hnr.shop=e.attribute("v",hnr.shop);
+						} else if(e.attribute("k","")=="name"
+						          || e.attribute("k","")=="addr:housename") {
+							hnr.name=e.attribute("v",hnr.name);
+						} else if(e.attribute("k","").toLower()=="fixme") {
+							hnr.fixme=true;
+						}
+					} else if(e.tagName()=="nd" && hnr.lat==-1) {
+						vGetLatLonForWay(hnr, e.attribute("ref","0").toInt());
+					}
+					
+				} // if(!e.isNull())
+				
+				n=n.nextSibling();
+				
+			} // while(!n.isNull())
+			
+			if(isComplete(hnr)) {
+				if(bFindDupe(hnr)) {
+					qDebug() << 100*i/dnlNodes.length() << "- Dupe found!";
+					qlDupes.append(hnr);
+				} else {
+					qlHousenumbers.append(hnr);
+				}
+			} else {
+				//qDebug() << "There is something wrong with this element";
+				//qDebug() << hnr.lat << hnr.lon << hnr.id << hnr.country << hnr.city << hnr.street << hnr.number << hnr.fixme;
+			}
+			
+		} // for(dnlNodes)
+	} // for(nodes/ways)
 	
+	duplicatesStream << "lat\tlon\ttitle\tdescription\ticon\ticonSize\ticonOffset\n";
 	for(int i=0; i<qlDupes.length(); i++) {
-		qDebug() << qlDupes[i].id << qlDupes[i].city << qlDupes[i].housenumber << "id dupe of" << qlDupes[i].dupeId;
+		qDebug() << qsGenerateOutput(qlDupes[i]);
+		duplicatesStream << qsGenerateOutput(qlDupes[i]);
 	}
 	
-	qDebug() <<  "finished after" <<  now.elapsed()/1000 << "seconds";
+	duplicatesFile.close();
+	
+	qDebug() << "finished after" <<  now.elapsed()/1000 << "seconds";
+	qDebug() << qlHousenumbers.length() << "housenumbers," << qlDupes.length() << "dupes";
 }
 
 bool bFindDupe(housenumber &hnr) {
 	for(int i=0; i<qlHousenumbers.count(); i++) {
-		if(qlHousenumbers[i].housenumber==hnr.housenumber && qlHousenumbers[i].street==hnr.street &&
-		   qlHousenumbers[i].city==hnr.city && qlHousenumbers[i].postcode==hnr.postcode && qlHousenumbers[i].country==hnr.country) {
-			hnr.dupeId=qlHousenumbers[i].id;
+		if(qlHousenumbers[i].number==hnr.number && qlHousenumbers[i].street==hnr.street &&
+		   qlHousenumbers[i].city==hnr.city && qlHousenumbers[i].postcode==hnr.postcode &&
+		   qlHousenumbers[i].name==hnr.name &&
+		   qlHousenumbers[i].country==hnr.country && qlHousenumbers[i].shop==hnr.shop) {
+			hnr.dupe=&qlHousenumbers[i];
 			return true;
 		}
 	}
 	return false;
+}
+
+bool isComplete(housenumber hnr) {
+	if(hnr.id==0 || hnr.lat==0 || hnr.lon==0 || hnr.country=="" || hnr.city=="" || hnr.postcode=="" || hnr.number=="" || (hnr.fixme==true && bIgnoreFixme) ) return false;
+	return true;
+}
+
+QString qsGenerateOutput(housenumber hnr) {
+	QString link=QString("<a href=\"http://www.openstreetmap.org/browse/%1/%2\">%2</a> (<a href=\"http://localhost:8111/load_and_zoom?left=%5&right=%6&top=%4&bottom=%3&select=%1%2\">JOSM</a>)")
+	                .arg(hnr.isWay ? "way" : "node")
+	                .arg(hnr.id)
+	                .arg(hnr.lat-0.000001,0,'f',7).arg(hnr.lat+0.000001,0,'f',7)
+	                .arg(hnr.lon-0.000001,0,'f',7).arg(hnr.lon+0.000001,0,'f',7);
+	
+	QString dupeLink=QString("<a href=\"http://www.openstreetmap.org/browse/%1/%2\">%2</a> (<a href=\"http://localhost:8111/load_and_zoom?left=%5&right=%6&top=%4&bottom=%3&select=%1%2\">JOSM</a>)")
+	                .arg(hnr.dupe->isWay ? "way" : "node")
+	                .arg(hnr.dupe->id)
+	                .arg(hnr.dupe->lat-0.000001,0,'f',7).arg(hnr.dupe->lat+0.000001,0,'f',7)
+	                .arg(hnr.dupe->lon-0.000001,0,'f',7).arg(hnr.dupe->lon+0.000001,0,'f',7);
+	
+	return QString("%1\t%2\tDupe\t%3 %4 %5 %6 %7 %8 is dupe of %9 \tpin.png\t16,16\t-8,-8\n")
+	                .arg(hnr.lat,0,'f',8).arg(hnr.lon,0,'f',8).arg(link)
+	                .arg(hnr.country).arg(hnr.city).arg(hnr.postcode).arg(hnr.street)
+	                .arg(hnr.number).arg(dupeLink);
+}
+
+void vGetLatLonForWay(housenumber &hnr, int ref) {
+	for(int i=0; i<qlNodes.count(); i++) {
+		if(qlNodes[i].id==ref) {
+			hnr.lat=qlNodes[i].lat;
+			hnr.lon=qlNodes[i].lon;
+		}
+	}
 }
