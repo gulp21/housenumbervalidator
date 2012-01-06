da847bd6fbe3582368f06460558bcf3b43b6acfd
gulp21 <support.gulp21@googlemail.com>
2012-01-06 17:58:46 +0100

    NRW update 06/01/12

--- html/counter.php
+++ html/counter.php
@@ -1,7 +1,18 @@
 <?
+
+// in case do not track is enabled, save a shortened ip
+function ip() {
+	$addr=$_SERVER['REMOTE_ADDR'];
+	if(strlen($addr)<3) return "NAN";
+	if(isset($_SERVER['HTTP_DNT']) && $_SERVER['HTTP_DNT']==1) {
+		return "DNT".$addr[0].$addr[1].$addr[2].".".$addr[strlen($addr)-2].$addr[strlen($addr)-1];
+	} else {
+		return $addr;
+	}
+}
+
 $out = fopen("counter.txt", "a");
 $timestamp = date("ymd:H",time());
-$uhrzeit = date("H:i",$timestamp);
-fputs($out,$_SERVER['REMOTE_ADDR']." ".$_GET["id"]." ".$timestamp." ".$_SERVER['HTTP_USER_AGENT']."\n");
+fputs($out,ip()."\t".$_GET["id"]."\t".$timestamp."\t".$_SERVER['HTTP_USER_AGENT']."\n");
 fclose($out);
 ?>
