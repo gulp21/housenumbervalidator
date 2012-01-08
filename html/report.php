592d687d51b37f595ccb204af3fbe4457cc1de7a
gulp21 <support.gulp21@googlemail.com>
2012-01-08 15:48:41 +0100

    report: do not save id which has been already reported

--- html/report.php
+++ html/report.php
@@ -1,22 +1,27 @@
 <html>
 <body>
 <?
+if($_GET["way"]==="true") {
+	$str="\n"."way".trim($_GET["id"])."\"";
+} else {
+	$str="\n"."node".trim($_GET["id"])."\"";
+}
+$file = file_get_contents("reports.txt");
+if(strpos($file, $str)) {
+	print("Vielen Dank! Nach der n&auml;chsten Aktualisierung wird dieser Weg nicht mehr angezeigt. (Der Fehlalarm wurde bereits gemeldet.)");
+	exit;
+}
 $out = fopen("reports.txt", "a");
 if(!$out) {
-    print("Err");
-    exit;
+	print("Err");
+	exit;
 }
-if(!is_numeric($_GET["id"])) {
+if(!is_numeric(trim($_GET["id"]))) {
 	print("Bitte die ID aus dem Popup in das Textfeld eingeben.");
 	exit;
 }
-if($_GET["way"]==="true") {
-	fputs($out,"\n"."way".$_GET["id"]."\"");
-	print("Vielen Dank! Nach der n&auml;chsten Aktualisierung wird dieser Weg nicht mehr angezeigt.");
-} else {
-	fputs($out,"\n"."node".$_GET["id"]."\"");
-	print("Vielen Dank! Nach der n&auml;chsten Aktualisierung wird dieser Knoten nicht mehr angezeigt.");
-}
+fputs($out,$str);
+print("Vielen Dank! Nach der n&auml;chsten Aktualisierung wird dieser Fehler nicht mehr angezeigt.");
 fclose($out);
 ?>
 </body>
