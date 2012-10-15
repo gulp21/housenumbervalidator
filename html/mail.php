945cf21b096eb1522b1f1e0fc2c4b11e76d3fe45
gulp21 <support.gulp21@googlemail.com>
2012-10-15 12:33:10 +0200

    improved mail.php, so that mails are also sent when there are not enough problems

--- html/mail.php
+++ html/mail.php
@@ -1,7 +1,11 @@
 <?php
 	include("../connect.php");
 	
-	$skip=10;
+	$skip=10; // number of entries which are skipped, so that everyone (probably) gets a object from another area
+	
+	$i=0; // number of elements in the mailContents array
+	
+	$VERY_NEAR_THRESHOLD=0.00002;
 	
 	mysql_set_charset("utf8");
 	
@@ -11,23 +15,36 @@
 	
 	echo $subscribers." subscribers<br/>";
 	
-	$probs=mysql_query("SELECT * FROM problematic WHERE corrected=0 AND easyfix=1") or die ("MySQL-Error: ".mysql_error());
+	$probs=mysql_query("SELECT * FROM problematic
+	                    WHERE corrected=0 AND easyfix=1")
+	                    or die ("MySQL-Error: ".mysql_error());
 	
-	echo "Found ".mysql_num_rows($probs)." Ps<br/>";
+	echo "Found ".mysql_num_rows($probs)." problems<br/>";
 	
-	while(mysql_num_rows($probs) < $subscribers*$skip+$skip) {
+	while(mysql_num_rows($probs) < $subscribers*$skip+$skip && $skip!=0) {
 		echo $skip." that's not enough...<br/>";
-		if($skip==1) die("that's not enough... REALLY");
 		--$skip;
 	}
 	
-	$i=-rand(0,mysql_num_rows($probs)-$subscribers*$skip);
+	if($skip>1)
+		$startIndex=rand(0,mysql_num_rows($probs)-$subscribers*$skip);
+	else
+		$startIndex=0;
 	
-	echo "i ".$i."<br/>";
+	echo "startIndex ".$startIndex."<br/>";
 	
-	while($prob=mysql_fetch_assoc($probs)) {
+	$skipped=$skip;
+	
+	while(($prob=mysql_fetch_assoc($probs)) && $i<$subscribers) {
 		
-		if($i++<0) continue;
+		if($startIndex-->0) continue;
+		
+		if($skipped!=$skip) {
+			++$skipped;
+			continue;
+		}
+		
+		$skipped=0;
 		
 		$table="";
 		
@@ -59,31 +76,44 @@
 		
 		$link="OSM: http://www.openstreetmap.org/browse/".$type."/".$prob['id']."\nJOSM: http://localhost:8111/load_and_zoom?left=".($prob['lon']-0.001)."&right=".($prob['lon']+0.001)."&top=".($prob['lat']+0.001)."&bottom=".($prob['lat']-0.001)."&select=".$type.$prob['id']."\nPotlatch 2: http://www.openstreetmap.org/edit?zoom=18&".$type."=".$prob['id']."&editor=potlatch2";
 		
-		$problems[$i]=
+		$mailContents[$i]=
 			"Problematisch\n"
 			.$link."\n"
 			.$table
 			."\n";
 		
-		if($i==$subscribers*$skip) break;
+		++$i;
 	}
 	
-	$VERY_NEAR_THRESHOLD=0.00002;
+	$dupes=mysql_query("SELECT * FROM dupes
+	                    WHERE corrected=0 AND possible_dupe=0
+	                    AND (lat-dupe_lat)<$VERY_NEAR_THRESHOLD and (lon-dupe_lon)<$VERY_NEAR_THRESHOLD
+	                    and (lat-dupe_lat)>-$VERY_NEAR_THRESHOLD and (lon-dupe_lon)>-$VERY_NEAR_THRESHOLD")
+	                    or die ("MySQL-Error: ".mysql_error());
 	
-	$dupes=mysql_query("SELECT * FROM dupes WHERE corrected=0 AND possible_dupe=0 AND (lat-dupe_lat)<$VERY_NEAR_THRESHOLD and (lon-dupe_lon)<$VERY_NEAR_THRESHOLD and (lat-dupe_lat)>-$VERY_NEAR_THRESHOLD and (lon-dupe_lon)>-$VERY_NEAR_THRESHOLD") or die ("MySQL-Error: ".mysql_error());
+	echo "Found ".mysql_num_rows($dupes)." dupes<br/>";
 	
-	echo "Found ".mysql_num_rows($dupes)." Ds<br/>";
+	$skip=10;
 	
-	if(mysql_num_rows($dupes) < $subscribers*$skip+$skip)
-                die("that's not enough...");
+	if(mysql_num_rows($dupes) < $subscribers*$skip+$skip+($subscribers-$i))
+		die("that's not enough...");
 	
-	$i=-rand(0,mysql_num_rows($dupes)-$subscribers*$skip);
+	$startIndex=rand(0,mysql_num_rows($dupes)-$subscribers*$skip);
 	
-	echo "i ".$i."<br/>";
+	echo "startIndex ".$startIndex."<br/>";
 	
-	while($dupe=mysql_fetch_assoc($dupes)) {
+	$skipped=$skip;
+	
+	while(($dupe=mysql_fetch_assoc($dupes)) && $i<$subscribers*2) {
 		
-		if($i++<0) continue;
+		if($startIndex-->0) continue;
+		
+		if($skipped!=$skip) {
+			++$skipped;
+			continue;
+		}
+		
+		$skipped=0;
 		
 		$table="";
 		
@@ -111,24 +141,24 @@
 		
 		$link.="\nOSM: http://www.openstreetmap.org/browse/".$type_dupe."/".$dupe['dupe_id']."\nJOSM: http://localhost:8111/load_and_zoom?left=".($dupe['dupe_lon']-0.001)."&right=".($dupe['dupe_lon']+0.001)."&top=".($dupe['dupe_lat']+0.001)."&bottom=".($dupe['dupe_lat']-0.001)."&select=".$type_dupe.$dupe['dupe_id']."\nPotlatch 2: http://www.openstreetmap.org/edit?zoom=18&".$type_dupe."=".$dupe['dupe_id']."&editor=potlatch2";
 		
-		$duplicates[$i]=
+		$mailContents[$i]=
 			"Duplikat\n"
 			.$link."\n"
 			.$table
 			."\n\n";
 		
-		if($i==$subscribers*$skip) break;
+		++$i;
 	}
 	
-	while($mail=mysql_fetch_assoc($mails)) {
-		mail($mail['mail'], "Ein korrigierter Fehler am Tag", "Wie wär's heute mit diesem kleinen Fehler:\n\n".$problems[$i]."Und wenn noch etwas Zeit ist:\n".$duplicates[$i]."--\nBei Problemen oder Fragen oder wenn Sie sich abmelden wollen, besuchen Sie bitte http://gulp21.bplaced.net/osm/housenumbervalidator und nutzen Sie die angegebenen Kontaktmöglichkeiten oder den Link \"Ein korrigierter Fehler am Tag\" zum Abmelden.", "Content-Type: text/plain; charset=\"utf-8\"\nFrom: housenumbervalidator <support.gulp21@googlemail.com>");
+	for($j=0; $mail=mysql_fetch_assoc($mails); ++$j) {
+		mail($mail['mail'], "Ein korrigierter Fehler am Tag", "Wie wär's heute mit diesem kleinen Fehler:\n\n".$mailContents[$j]."Und wenn noch etwas Zeit ist:\n".$mailContents[$j+$subscribers]."--\nBei Problemen oder Fragen oder wenn Sie sich abmelden wollen, besuchen Sie bitte http://gulp21.bplaced.net/osm/housenumbervalidator und nutzen Sie die angegebenen Kontaktmöglichkeiten oder den Link \"Ein korrigierter Fehler am Tag\" zum Abmelden.", "Content-Type: text/plain; charset=\"utf-8\"\nFrom: housenumbervalidator <support.gulp21@googlemail.com>");
 		
-		$problems[$i]=explode("\n",$problems[$i]);
-		$duplicates[$i]=explode("\n",$duplicates[$i]);
+		$first=explode("\n",$mailContents[$j]);
+		$second=explode("\n",$mailContents[$j+$subscribers]);
 		
-		echo "<br/>".$mail['mail']."<br/>".$problems[$i][1]."<br/>".$duplicates[$i][1]."<br/>";
-		
-		$i-=$skip;
+		echo "<br/>".$mail['mail']."<br/>"
+		     .$first[0]." ".$first[1]."<br/>"
+		     .$second[0]." ".$second[1]."<br/>";
 	}
 	
 ?>
